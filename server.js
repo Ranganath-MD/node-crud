db9761ed84529efc207d9ad1c051b2de4e60d884
Ranganath-MD <ranganath.developer@gmail.com>
2020-01-26 12:15:40 +0530

    added index.html

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express")
+var path = require('path');
 const cors = require('cors')
 var port = process.env.PORT || 3005
 const { mongoose } = require('./config/database')
@@ -9,8 +10,8 @@ app.use(express.json())
 app.use(cors())
 app.use("/users", usersRouter)
 
-app.get('/', function (req, res) {
-    res.send("CRUD app using Node");
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 app.listen(port, () => {
